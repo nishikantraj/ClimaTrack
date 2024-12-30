8a17511ca044856e41cf9bebf47344a5b72f1280
nishikantraj <nishikantraj1977@gmail.com>
2024-12-30 15:05:23 +0530

    Created all the necessary functions for fetching the data from open source api

--- src/api/weather.ts
+++ src/api/weather.ts
@@ -1,5 +1,6 @@
 
 import { API_CONFIG } from "./config";
+import { Cordinates, ForeCastData, GeoCodingResponse, WeatherData } from "./types";
 
 class WeatherAPI{
 
@@ -11,25 +12,43 @@ class WeatherAPI{
         return `${endpoint}?${searchParams.toString()}` 
     }
 
+    // to check whether response.json is the correct form of return or await??
     private async fetchData<T>(url:string):Promise<T>{
         const response = await fetch(url);
 
         if(!response.ok){
             throw new Error(`Weather API Error: ${response.statusText}`)
         }
-        return response.json()
+        return await response.json()
     }
 
-    async getCurrentWeather(){
-
+    async getCurrentWeather({lat, lon}:Cordinates):Promise<WeatherData>{
+        const url = this.createUrl(`${API_CONFIG.BASE_URL}/weather`,{
+            lat:lat.toString(),
+            lon:lon.toString(),
+            units:API_CONFIG.DEFAULT_PARAMS.units
+        })
+        return this.fetchData<WeatherData>(url);
     }
 
-    async getForecast(){
-
+    async getForecast({lat, lon}:Cordinates):Promise<ForeCastData>{
+        const url = this.createUrl(`${API_CONFIG.BASE_URL}/forecast`,{
+            lat:lat.toString(),
+            lon:lon.toString(),
+            units:API_CONFIG.DEFAULT_PARAMS.units
+        })
+        return this.fetchData<ForeCastData>(url);
     }
 
-    async reverseGeoCode(){
-
+    async reverseGeoCode({lat, lon}:Cordinates):Promise<GeoCodingResponse[]>{
+        const url = this.createUrl(`${API_CONFIG.GEO}/reverse`,{
+            lat:lat.toString(),
+            lon:lon.toString(),
+            limit:1
+        })
+        return this.fetchData<GeoCodingResponse[]>(url);
     }
 
 }
+
+export const weatherAPI = new WeatherAPI;
