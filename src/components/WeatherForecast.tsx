d41e105d566b5ff7b2521d9177215b555fa6bc00
nishikantraj <nishikantraj1977@gmail.com>
2024-12-31 15:12:38 +0530

    Fixed the overlapping problem of Forecast section for smaller devices

--- src/components/WeatherForecast.tsx
+++ src/components/WeatherForecast.tsx
@@ -59,7 +59,7 @@ const WeatherForecast = ({data}:WeatherForecastProps) => {
                             </div>
 
                             <div>
-                                <div className="flex justify-center gap-4">
+                                <div className="flex justify-center gap-4 flex-wrap">
                                     <span className="flex items-center text-blue-500">
                                         <ArrowDown className="mr-1 h-4 w-4"/>
                                         {formatTemp(day.temp_min)}
@@ -71,7 +71,7 @@ const WeatherForecast = ({data}:WeatherForecastProps) => {
                                 </div>
                             </div>
 
-                            <div className="flex justify-end gap-4">
+                            <div className="flex justify-end gap-4 flex-wrap">
                                 <span className="flex items-center gap-1">
                                     <Droplets className="h-4 w-4 text-blue-500"/>
                                     <span className="text-sm">
